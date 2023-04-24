93882ad04c09020f14b8f05b7ce6a991ff95b35c
vanhuu1904 <114399648+vanhuu1904@users.noreply.github.com>
2023-04-25 00:28:56 +0700

    Add files via upload

---
+++ week1/QuadraticEquation.java
@@ -0,0 +1,33 @@
+package week1;
+import java.util.Scanner;
+public class QuadraticEquation {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of a ");
+        double a = sc.nextDouble();
+        
+        System.out.print("Enter the value of b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Enter the value of c: ");
+        double c = sc.nextDouble();
+
+        double denta = b * b - 4 * a * c;
+
+        if(a == 0){
+            System.out.println("Error: a should not be zero");
+        }else if (denta < 0){
+            System.out.println("The equation has no real solution");
+        }else if(denta == 0){
+            double x = (-b) / (2 * a);
+            System.out.println("The equation has a double solution x = "+ x);
+        }else {
+            double x1 = (-b + Math.sqrt(denta)) / ( 2 * a);
+            double x2 = (-b - Math.sqrt(denta)) / ( 2 * a);
+            System.out.println("The equation has two solutions x1 =" + x1 + " and x2 = " + x2);
+        }
+	}
+
+}
